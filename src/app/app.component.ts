21386f0f89fdf86eb7a1ddb45039d85e03eaf53e
omar571mamdouh <ok1119076@gmail.com>
2025-03-16 12:43:20 +0200

    landing page building

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CardComponent } from './shared/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent , RouterOutlet , FooterComponent , CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
